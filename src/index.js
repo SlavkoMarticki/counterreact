063e37274757beaaced46c8e43fb3438ef3af661
Slavko <martickis@gmail.com>
2022-07-24 15:18:08 +0200

    CounterReact

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const Holder = () => {
+   
+    const [count, setCount] = React.useState(0);
+
+    const plus = () =>{
+        setCount(count + 1);
+    }
+
+    const minus = () => {
+        setCount(count - 1);
+    }
+
+    return(
+    <div className="holder">
+        <div className="numberHolder">
+            <p>{count}</p>
+        </div>
+        <div className="buttonHolder">
+            <button onClick={plus}>+</button>
+            <button onClick={minus}>-</button>
+        </div>
+    </div>);
+}
+const Container = () => {
+    return(
+        <div className="Container">
+            <Holder />
+        </div>
+    );
+}
+
+root.render(<Container />)
+
+
